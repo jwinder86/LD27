7394cc78ff0c96ada919f75691dde867d95cf4bc
sandaz13 <mike@sanders-tech.com>
2013-08-24 01:10:35 -0500

    updated boredom counter

--- Assets/Scripts/BoredomClock.cs
+++ Assets/Scripts/BoredomClock.cs
@@ -3,28 +3,36 @@ using System.Collections;
 
 public class BoredomClock : MonoBehaviour {
 	
-	float boredom = 10;
+	float boredom;
+	float boredomMax = 10;
+	float smallIncrease = 5;
+	float largeIncrease = 10;
 	
 	// Use this for initialization
 	void Start () {
-	
+		boredom = boredomMax;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	 	boredom = boredom - Time.smoothDeltaTime;
+	 	
+		if(boredom > 0){
+			boredom = boredom - Time.smoothDeltaTime;
+		}
 		Debug.Log(boredom);
 		
+		// TODO: need to update this block to be a real trigger thing
 		if(Input.GetButton("Jump")){
-			boredom += 5;
+			boredom += smallIncrease;
 			Debug.Log("more time");
 		}
 		
-		if(boredom > 10){
-			boredom = 10;
+		// don't allow the boredom clock to be higher than boredomMax
+		if(boredom > boredomMax){
+			boredom = boredomMax;
 		}
 		
-		if (boredom < 0){
+		if (boredom <= 0.01f){
 			Debug.Log("DEAD");
 		}
 	}
