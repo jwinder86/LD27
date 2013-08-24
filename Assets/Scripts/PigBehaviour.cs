1c67c0f1091432534f4c3b0efcc69352aacf881a
John Winder <jwinder86@gmail.com>
2013-08-24 00:11:21 -0700

    Pigs

---
+++ Assets/Scripts/PigBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(Collider))]
+public class PigBehaviour : MonoBehaviour {
+	
+	public float runSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// move left and right
+		if (Input.GetAxis("Horizontal") < 0f) {
+			rigidbody.velocity = new Vector3(-runSpeed, rigidbody.velocity.y, 0f);
+		} else if (Input.GetAxis("Horizontal") > 0f) {
+			rigidbody.velocity = new Vector3(runSpeed, rigidbody.velocity.y, 0f);
+		}
+	}
+}
