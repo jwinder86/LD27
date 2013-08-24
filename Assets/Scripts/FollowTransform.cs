51be3fba3caac967b7e59d6867423b7534d5f7f8
John Winder <jwinder86@gmail.com>
2013-08-23 23:21:01 -0700

    Camera follows rocket

--- Assets/Scripts/FollowTransform.cs
+++ Assets/Scripts/FollowTransform.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class FollowTransform : MonoBehaviour {
-
+	
+	public Transform toFollow;
+	
+	private Vector3 diff;
+	
 	// Use this for initialization
 	void Start () {
-	
+		diff = transform.position - toFollow.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+		transform.position = toFollow.position + diff;
 	}
 }
