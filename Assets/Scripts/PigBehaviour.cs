3a8a1d2168bf7344942bfb1b4fd7a148dc7b5adf
John Winder <jwinder86@gmail.com>
2013-08-24 00:20:18 -0700

    Pig Walk/Jump

--- Assets/Scripts/PigBehaviour.cs
+++ Assets/Scripts/PigBehaviour.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class PigBehaviour : MonoBehaviour {
 	
 	public float runSpeed;
+	public float jumpSpeed;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,11 @@ public class PigBehaviour : MonoBehaviour {
 			rigidbody.velocity = new Vector3(-runSpeed, rigidbody.velocity.y, 0f);
 		} else if (Input.GetAxis("Horizontal") > 0f) {
 			rigidbody.velocity = new Vector3(runSpeed, rigidbody.velocity.y, 0f);
+		}
+		
+		// jump
+		if (Input.GetButtonDown("Jump")) {
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed, 0f);
 		}
 	}
 }
