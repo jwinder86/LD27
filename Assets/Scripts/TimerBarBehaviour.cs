e0bfd8f6a4d6c6be26f1a6b0628a68bdb02f19e2
John Winder <jwinder86@gmail.com>
2013-08-24 14:57:44 -0700

    Timer Bar

---
+++ Assets/Scripts/TimerBarBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerBarBehaviour : MonoBehaviour {
+	
+	public Transform bar;
+	public TextMesh text;
+	
+	public Color fullColor;
+	public Color emptyColor;
+	
+	private Vector3 initialScale;
+	
+	// Use this for initialization
+	void Start () {
+		initialScale = bar.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void setStatus(float fraction, float value) {
+		bar.transform.localScale = new Vector3(initialScale.x * fraction, initialScale.y, initialScale.z);
+		bar.renderer.material.color = Color.Lerp(emptyColor, fullColor, fraction);
+		text.text = value.ToString("F2");
+	}
+}
