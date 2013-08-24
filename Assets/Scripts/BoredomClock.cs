8273d83d9af6ab5bc9fb0a22769917a8cde057ef
sandaz13 <mike@sanders-tech.com>
2013-08-23 23:49:39 -0500

    added simple version one of boredom clock

---
+++ Assets/Scripts/BoredomClock.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoredomClock : MonoBehaviour {
+	
+	float boredom = 10;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 	boredom = boredom - Time.smoothDeltaTime;
+		Debug.Log(boredom);
+		
+		if(Input.GetButton("Jump")){
+			boredom += 5;
+			Debug.Log("more time");
+		}
+		
+		if(boredom > 10){
+			boredom = 10;
+		}
+		
+		if (boredom < 0){
+			Debug.Log("DEAD");
+		}
+	}
+}
