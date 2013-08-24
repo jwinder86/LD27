6381970ab2f21dbccd978d829ec8d3f1425e8690
sandaz13 <mike@sanders-tech.com>
2013-08-24 16:54:50 -0500

    added adrenalie syrniges

---
+++ Assets/Scripts/SyringeTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyringeTrigger : MonoBehaviour {
+	
+	public BoredomClock boredomClock;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	 void OnTriggerEnter(Collider other) {
+		if(null != other.GetComponent("PigBehaviour")){
+			boredomClock.increaseClock(10);
+			Destroy(gameObject);
+		}
+    }
+	
+	
+}
